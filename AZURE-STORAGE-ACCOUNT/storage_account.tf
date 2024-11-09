bb3bad5a0c3fe7ccc55f15da0468e41fc8f804df
Felipe Dias <fsdiasdf@gmail.com>
2024-11-09 11:32:54 -0300

    adding new variables and apply command terraform init and terraform fmt for format code

--- AZURE-STORAGE-ACCOUNT/storage_account.tf
+++ AZURE-STORAGE-ACCOUNT/storage_account.tf
@@ -1,21 +1,21 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = ""
+  name     = var.resource_group_name
   location = var.location
 
   tags = local.common_tags
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = ""
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
   tags = local.common_tags
-}  
+}
 
 resource "azurerm_storage_container" "storage_container" {
-  name                  = ""
-  storage_account_name  = azurerm_storage_account.storage_account.name
+  name                 = var.container_name
+  storage_account_name = azurerm_storage_account.storage_account.name
 }
