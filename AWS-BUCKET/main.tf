2e36487cdc168cae350d7e49d19551f98d8f62d3
Felipe Dias <fsdiasdf@gmail.com>
2024-11-09 12:25:35 -0300

    creating code for AWS-Bucket and adding main.tf file

---
+++ AWS-BUCKET/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = ">= 1.9.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = sa-east-1
+
+  default_tags {
+    tags = {
+      owner = "felipedias"
+      managed-by = "terraform"
+    }
+  }
+}
