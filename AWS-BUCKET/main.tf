344a40e3ccb7077d4702907eff4049643867680e
Felipe Dias <fsdiasdf@gmail.com>
2024-11-09 12:52:47 -0300

    format file main.tf

--- AWS-BUCKET/main.tf
+++ AWS-BUCKET/main.tf
@@ -1,19 +1,19 @@
 terraform {
-    required_version = ">= 1.9.0"
+  required_version = ">= 1.9.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.74.0"
     }
   }
 }
 
 provider "aws" {
-  region = sa-east-1
+  region = "sa-east-1"
 
   default_tags {
     tags = {
-      owner = "felipedias"
+      owner      = "felipedias"
       managed-by = "terraform"
     }
   }
