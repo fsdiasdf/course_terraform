adf7f15a6f02c41cd0f5c89c3a58a05cf802e0f4
Felipe Dias <fsdiasdf@gmail.com>
2024-11-11 16:44:09 -0300

    project for VM creation in AZURE provider and include main.tf with block terraform remote state

---
+++ AZURE-VM/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_version = ">= 1.9.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.9.0"
+    }
+  }
+
+ backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "felipediasterraformstate"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "remote-state"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "azure-vm/terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "felipediasterraformstate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
