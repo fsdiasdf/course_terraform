6bd0cd54557c1cd0424e722bbcda1f50011be0c7
Felipe Dias <fsdiasdf@gmail.com>
2024-11-11 14:44:01 -0300

    include s3 backend and data source configuration

--- AWS-VM-COM-VPC-REMOTE-STATE/main.tf
+++ AWS-VM-COM-VPC-REMOTE-STATE/main.tf
@@ -1,18 +1,35 @@
 terraform {
     required_version = ">= 1.9.0"
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.75.1"
     }
   }
+
+  backend "s3" {
+    bucket = "felipedias-remote-state"
+    key    = "aws-vm/terraform.tfstate"
+    region = "sa-east-1"
+  }
 }
 
 provider "aws" {
+    region = "sa-east-1"
   default_tags {
     tags = {
       owner = "felipedias"
       managed-by = "terraform"
     }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "felipedias-remote-state"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "sa-east-1"
   }
 }
