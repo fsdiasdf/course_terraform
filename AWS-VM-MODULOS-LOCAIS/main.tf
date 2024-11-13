6b2be2ca134e0f81b2fbf2dd374ceef789f5e19b
Felipe Dias <fsdiasdf@gmail.com>
2024-11-13 14:59:18 -0300

    change subnet ip (not working in 10.0.1.0/24)

--- AWS-VM-MODULOS-LOCAIS/main.tf
+++ AWS-VM-MODULOS-LOCAIS/main.tf
@@ -28,6 +28,6 @@ provider "aws" {
 module "network" {
   source      = "./network"
   cidr_vpc    = "10.0.0.0/16"
-  cidr_subnet = "10.0.1.0/24"
+  cidr_subnet = "10.0.2.0/24"
   environment = var.environment
 }
