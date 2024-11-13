8487ccbbee9820b379fffe84aa994ed24d9857a2
Felipe Dias <fsdiasdf@gmail.com>
2024-11-13 09:56:20 -0300

    format code and update gitignore

--- AZURE-VM-CUSTOM-DATA/main.tf
+++ AZURE-VM-CUSTOM-DATA/main.tf
@@ -8,9 +8,9 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"         # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "felipediasterraformstate"   # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "remote-state"               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    resource_group_name  = "rg-terraform-state"                     # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "felipediasterraformstate"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "remote-state"                           # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "azure-vm-custom-data/terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
