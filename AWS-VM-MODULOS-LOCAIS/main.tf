e56c361f0ebea5441e796b932d4ac9c47dfb7a20
Felipe Dias <fsdiasdf@gmail.com>
2024-11-13 14:35:59 -0300

    creating project using local modules

---
+++ AWS-VM-MODULOS-LOCAIS/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "felipedias-remote-state"
+    key    = "aws-vm-modulos-locais/terraform.tfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+  default_tags {
+    tags = {
+      owner      = "felipedias"
+      managed-by = "terraform"
+    }
+  }
+}
+
+module "network" {
+  source = "./network"
+  cidr_vpc = "10.0.0.0/16"
+  cidr_subnet = "10.0.1.0/24"
+  environment = var.environment
+}
