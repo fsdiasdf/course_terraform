043bcf2cfd1168b12b18c2cfaea151db20e88b0d
Felipe Dias <fsdiasdf@gmail.com>
2024-11-19 18:42:59 -0300

    update attribute sas_token for sensitive=true

--- data-source-azure-storage-account/outputs.tf
+++ data-source-azure-storage-account/outputs.tf
@@ -1,6 +1,7 @@
 output "sas_token" {
   description = "SAS Token para container de imagens"
   value       = data.azurerm_storage_account_blob_container_sas.sas_token.sas
+  sensitive = true
 }
 
 output "container_url" {
